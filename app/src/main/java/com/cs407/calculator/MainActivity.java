e9ae34d6fdce670b7b7e81b4721d74f9b126bf54
Maddie66 <caimadison07@gmail.com>
2023-09-25 13:09:58 -0500

    milestone2

--- app/src/main/java/com/cs407/calculator/MainActivity.java
+++ app/src/main/java/com/cs407/calculator/MainActivity.java
@@ -2,7 +2,11 @@ package com.cs407.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,45 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void addition(View view){
+        EditText integer1 = (EditText) findViewById(R.id.integer1);
+        EditText integer2 = (EditText) findViewById(R.id.integer2);
+        Double int1 = Double.parseDouble(integer1.getText().toString());
+        Double int2 = Double.parseDouble(integer2.getText().toString());
+        Double result = int1 + int2;
+        goToActivity(Double.toString(result));
+    }
+    public void subtraction(View view){
+        EditText integer1 = (EditText) findViewById(R.id.integer1);
+        EditText integer2 = (EditText) findViewById(R.id.integer2);
+        Double int1 = Double.parseDouble(integer1.getText().toString());
+        Double int2 = Double.parseDouble(integer2.getText().toString());
+        Double result = int1 - int2;
+        goToActivity(Double.toString(result));
+    }
+
+    public void multiplication(View view){
+        EditText integer1 = (EditText) findViewById(R.id.integer1);
+        EditText integer2 = (EditText) findViewById(R.id.integer2);
+        Double int1 = Double.parseDouble(integer1.getText().toString());
+        Double int2 = Double.parseDouble(integer2.getText().toString());
+        Double result = int1 * int2;
+        goToActivity(Double.toString(result));
+    }
+
+    public void division(View view){
+        EditText integer1 = (EditText) findViewById(R.id.integer1);
+        EditText integer2 = (EditText) findViewById(R.id.integer2);
+        Double int1 = Double.parseDouble(integer1.getText().toString());
+        Double int2 = Double.parseDouble(integer2.getText().toString());
+        Double result = int1 / int2;
+        goToActivity(Double.toString(result));
+    }
+    public void goToActivity(String s){
+        Intent intent=new Intent(this, Result.class);
+        intent.putExtra("result",s);
+        startActivity(intent);
     }
 }
